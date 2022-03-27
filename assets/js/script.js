182d4cec501f2ed8d7ae87acbc16e8f2825d15f9
Victor Lawal <Viclawal@aol.com>
2022-03-27 13:07:31 -0700

    edit readme, add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
 // Assignment code here
+//Characters used in password
 var lowerc = "abcdefghijklmnopqrstuvwxyz";
 var upperc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var symbols = "!@#$%^&*()_-+=";
+var password = "";
 
 window.alert("For previously generated passwords check console (ctrl + shift i)")
 
+//Generate password
 function generatePassword(length, characters) {
   let password = "";
   for (var i = 0; i < length; i++) {
@@ -33,14 +36,17 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   let characters = "";
-
+  //Check what criteria the user prompted
   incLowerc.checked ? (characters += lowerc) : "";
   incUpperc.checked ? (characters += upperc) : "";
   incNumbers.checked ? (characters += numbers) : "";
   incSymbols.checked ? (characters += symbols) : "";
+
+  //Generate password with selected criteria
   passwordText.value = generatePassword(length.value, characters);
 
-console.log(passwordText.value)
+  //Save previous passwords to console
+  console.log(passwordText.value)
 }
 
 // Add event listener to generate button
