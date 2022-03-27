db95c6ca480a8d065c0d968ef148d9e0e46ae71d
Victor Lawal <Viclawal@aol.com>
2022-03-27 12:53:56 -0700

    add password generator

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Password Generator Starter Code
+## Coded by Victor Lawal
 
 ## Product Link
 
@@ -6,28 +7,8 @@
 
 ## Screenshot
 
-![Screenshot of working product]()
+![Screenshot of working product](./assets/images/password-gen.jpg)
 
-## User Story
+## Description
 
-### AS AN employee with access to sensitive data
-### I WANT to randomly generate a password that meets certain criteria
-### SO THAT I can create a strong password that provides greater security
-
-## Criteria
-
-### GIVEN I need a new, secure password
-### WHEN I click the button to generate a password
-### THEN I am presented with a series of prompts for password criteria
-### WHEN prompted for password criteria
-### THEN I select which criteria to include in the password
-### WHEN prompted for the length of the password
-### THEN I choose a length of at least 8 characters and no more than 128 characters
-### WHEN asked for character types to include in the password
-### THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-### WHEN I answer each prompt
-### THEN my input should be validated and at least one character type should be selected
-### WHEN all prompts are answered
-### THEN a password is generated that matches the selected criteria
-### WHEN the password is generated
-### THEN the password is either displayed in an alert or written to the page
+### This web application is a random password generator which will ask the user to select the criteria they need for their password and generate it randomly.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,49 @@
 // Assignment code here
+var lowerc = "abcdefghijklmnopqrstuvwxyz";
+var upperc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*()_-+=";
+
+window.alert("For previously generated passwords check console (ctrl + shift i)")
+
+function generatePassword(length, characters) {
+  let password = "";
+  for (var i = 0; i < length; i++) {
+    password += characters.charAt(
+      Math.floor(Math.random() * characters.length)
+    );
+  }
+  return password;
+}
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passwordText = document.getElementById("password");
+var length = document.getElementById("length");
+var incLowerc = document.getElementById("lowerc");
+var incUpperc = document.getElementById("upperc");
+var incNumbers = document.getElementById("numbers");
+var incSymbols = document.getElementById("symbols");
+var generateBtn = document.getElementById("generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  let characters = "";
 
+  incLowerc.checked ? (characters += lowerc) : "";
+  incUpperc.checked ? (characters += upperc) : "";
+  incNumbers.checked ? (characters += numbers) : "";
+  incSymbols.checked ? (characters += symbols) : "";
+  passwordText.value = generatePassword(length.value, characters);
+
+console.log(passwordText.value)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
